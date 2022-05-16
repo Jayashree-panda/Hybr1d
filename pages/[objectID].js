c446e500bf96dcb82548acb89ffa573c4d840f6b
Jayashree-panda <jayashreepanda12345@gmail.com>
2022-05-16 08:08:34 +0530

    news details page

--- pages/[objectID].js
+++ pages/[objectID].js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import { useRouter } from "next/router";
-import { Loader, Segment } from 'semantic-ui-react';
+import { Loader, Segment, Icon } from 'semantic-ui-react';
 import axios from 'axios';
+import styles from '../styles/hacker_news.module.scss';
 
 export default function NewsDetails() {
     const [isLoading, setIsLoading] = useState(false);
-    const [newsDetails, setNewsDetails] = useState([]);
+    const [newsDetails, setNewsDetails] = useState({});
     const router = useRouter();
     const { objectID } = router.query;
 
@@ -14,7 +15,6 @@ export default function NewsDetails() {
             setIsLoading(true);
             axios.get(`https://hn.algolia.com/api/v1/items/${objectID}`)
             .then((res) => {
-                console.log(res.data)
                 setNewsDetails(res.data)
             })
             .catch((err) => alert(err))
@@ -24,11 +24,14 @@ export default function NewsDetails() {
 
     return (
         <div>
-            {isLoading ? <Loader active /> : <div>
-                <h1>{newsDetails?.title}</h1>
+            {isLoading && <Loader active />}
+             {!isLoading && Object.keys(newsDetails).length!==0 && <div  className={`${styles.hacker_news_container} ${styles.news_details_container}`}>
+                <h1>{newsDetails?.title || "No Title"}</h1>
+                <p className={styles.reward_points}><Icon name='star' />{newsDetails?.points || 0} points</p>
                 <h2>Comments...</h2>
                 {newsDetails?.children?.map((comment) => <Segment key={comment?.id}>
-                    <div dangerouslySetInnerHTML={{__html: comment?.text}} />
+                    {comment?.text ? <div className={styles.comment_text} dangerouslySetInnerHTML={{__html: comment?.text}} /> : <p className={styles.comment_text}>No Text</p>}
+                    <p className={styles.created_at}>{comment?.created_at}</p>
                 </Segment>)}
                 </div>}
         </div>
